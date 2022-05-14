038c074e61f6d8bc445bee8cf36cc209391deca8
Daria Fedde <dariafedde.it@gmail.com>
2022-05-14 15:25:32 +0200

    Nadmieniłam kod, według zaleceń promotora. Kod jest bardziej funkcjonalny. Musiałam usunąć plik danych z projketu, ponieważ był za duży do ppushowania

--- ProjektObrona_DariaFedde.py
+++ ProjektObrona_DariaFedde.py
@@ -1,105 +1,72 @@
 import pandas as pd
 import os
 import pathlib
-# # pd.set_option('display.max_rows',500)
-# pd.set_option('display.max_columns',50)
-# # szerokość DF (wyświetlanie)
-# pd.set_option('display.width',1000)
-# pd.set_option('display.max_colwidth', 100)
-#
-ROOT_DIR = os.path.dirname(__file__)
-#
-# print(ROOT_DIR)
-#
-df= pd.read_csv(os.path.join(ROOT_DIR, '../data/nypd-motor-vehicle-collisions.csv'))
-#
-# #df.head()
-#
-#
-#
-#
-# #ile osób zginęło na MANHATANIE
-#
-# # print('1. Ile osób zginęło na MANHATANIE?')
-# # dead_manhattan = df[df.BOROUGH == 'MANHATTAN']
-# #
-# #
-# # quantity = dead_manhattan['NUMBER OF PERSONS KILLED'].sum()
-# # print(quantity)
-#
-# def calculate_deaths_in_selected_borough(borough_name: str, nypd_collisions_data: pd.DataFrame) -> int:
-#     dead_manhattan = nypd_collisions_data[nypd_collisions_data.BOROUGH == borough_name]
-#     return dead_manhattan['NUMBER OF PERSONS KILLED'].sum().astype(int)
-#
-# # tutaj poniżej wywołanie funkcji ( print aby pokazać wartość którą zwraca)
-# print(calculate_deaths_in_selected_borough('MANHATTAN', df))
-#
-#
-#
-#
-#
-# #Jaka jest najczęstsza godzina wypadków
-#
-# print('2. Jaka jest najczęstsza godzina wypadków?')
-#
-# agg_accident_time_max = df.agg({'ACCIDENT TIME': 'max'})
-# print(agg_accident_time_max)
-#
-#
-#
-# #w jakich 5 stanach najwiecej wypadków
-#
-# print('3. W jakich 5 stanach najwiecej wypadków?')
-#
-# agg_accident_time_max = df.groupby('BOROUGH').agg({'BOROUGH': 'max'})
-# print(agg_accident_time_max)
-
-
-
-
 
 
 def calculate_deaths_in_selected_borough(borough_name: str, nypd_collisions_data: pd.DataFrame) -> int:
     dead_manhattan = nypd_collisions_data[nypd_collisions_data.BOROUGH == borough_name]
     return dead_manhattan['NUMBER OF PERSONS KILLED'].sum().astype(int)
 
-# tutaj poniżej wywołanie funkcji ( print aby pokazać wartość którą zwraca)
-print(calculate_deaths_in_selected_borough('MANHATTAN', df))
-
-
-
-
-
-# Jaka jest najczęstsza godzina wypadków
-print('2. Jaka jest najczęstsza godzina wypadków?')
 
 def calculate_agg_accident_time_max(nypd_collisions_data: pd.DataFrame):
     agg_accident_time_max = nypd_collisions_data.agg({'ACCIDENT TIME': 'max'})
     return agg_accident_time_max['ACCIDENT TIME']
+
+
+def calculate_agg_borough_max(nypd_collisions_data: pd.DataFrame, max_list: int = 5) -> list:
+    nypd_collisions_data["NUMBER OF PERSONS"] = nypd_collisions_data[f"NUMBER OF PERSONS INJURED"] + \
+                                                nypd_collisions_data[f"NUMBER OF PERSONS KILLED"]
+    agg_borough_max = nypd_collisions_data.groupby('BOROUGH').agg({'NUMBER OF PERSONS': 'max'})
+    borough_list = agg_borough_max.sort_values('NUMBER OF PERSONS', ascending=False).index.tolist()[:max_list]
+    return borough_list
+
+
+def calculate_agregate_number_accident_mean(nypd_collisions_data: pd.DataFrame, locomotion_type: str) -> float:
+    nypd_collisions_data[f"NUMBER OF {locomotion_type}"] = nypd_collisions_data[
+                                                               f"NUMBER OF {locomotion_type} INJURED"] + \
+                                                           nypd_collisions_data[f"NUMBER OF {locomotion_type} KILLED"]
+    agg_number_of_motorist_mean = nypd_collisions_data.agg({f'NUMBER OF {locomotion_type}': 'mean'})
+    return agg_number_of_motorist_mean[f'NUMBER OF {locomotion_type}'].round(2)
+
+
+def clalculate_accident_per_street_per_borough(nypd_collisions_data: pd.DataFrame, max_street_list: int = 5,
+                                               max_borough_list: int = 3):
+    nypd_collisions_data["NUMBER OF PERSONS"] = nypd_collisions_data[f"NUMBER OF PERSONS INJURED"] + \
+                                                nypd_collisions_data[f"NUMBER OF PERSONS KILLED"]
+    borough_list = calculate_agg_borough_max(nypd_collisions_data, max_borough_list)
+    dt_streets = nypd_collisions_data[nypd_collisions_data.BOROUGH.isin(borough_list)].groupby(['ON STREET NAME']).agg(
+        {'NUMBER OF PERSONS': 'sum'})
+    dt_streets_list = dt_streets.sort_values('NUMBER OF PERSONS', ascending=False).index.tolist()[:max_street_list]
+    return[street.replace(' ','') for street in dt_streets_list]
+
+
+# wczytanie pliku
+df = pd.read_csv(os.path.join(os.path.dirname(__file__), '../data/nypd-motor-vehicle-collisions.csv'))
+
+# wyciągnięcie danych
+print("1. ile osób zginęło na MANHATANIE")
+print(calculate_deaths_in_selected_borough('MANHATTAN', df))
+
+print("2. ile osób zginęło w QUEENS")
+print(calculate_deaths_in_selected_borough('QUEENS', df))
+
+print("3. ile osób zginęło w BROOKLYN")
+print(calculate_deaths_in_selected_borough('BROOKLYN', df))
+
+print('4. Jaka jest najczęstsza godzina wypadków?')
 print(calculate_agg_accident_time_max(df))
 
+print('5. W jakich 5 stanach najwiecej wypadków?')
+print(calculate_agg_borough_max(df, 5))
 
+print('6. Średnia ilość wypadków z udziałem pieszych')
+print(calculate_agregate_number_accident_mean(df, "PEDESTRIANS"))
 
-# w jakich 5 stanach najwiecej wypadków
-print('3. W jakich 5 stanach najwiecej wypadków?')
+print('7. Średnia ilość wypadków z udziałem rowerzystów')
+print(calculate_agregate_number_accident_mean(df, "CYCLIST"))
 
-def calculate_agg_borough_max(nypd_collisions_data: pd.DataFrame) -> list:
-    agg_borough_max = nypd_collisions_data.groupby('BOROUGH').agg({'BOROUGH': 'max'})
+print('8. Średnia ilość wypadków z udziałem motocyklistów')
+print(calculate_agregate_number_accident_mean(df, "MOTORIST"))
 
-    # print(type(agg_borough_max["BOROUGH"]))
-    return agg_borough_max['BOROUGH'].tolist()
-
-print(calculate_agg_borough_max(df))
-
-#Średnia ilość wypadków z udziałem motocyklisty
-
-print('4.Średnia ilość wypadków z udziałem motocyklisty')
-
-def calculate_agregate_number_of_motorist_min(nypd_collisions_data: pd.DataFrame) ->int:
-    nypd_collisions_data["NUMBER OF MOTORIST"] = nypd_collisions_data["NUMBER OF MOTORIST INJURED"] + \
-                                                 nypd_collisions_data["NUMBER OF MOTORIST KILLED"]
-    agg_number_of_motorist_mean = nypd_collisions_data.agg({'NUMBER OF MOTORIST': 'mean'})
-    return agg_number_of_motorist_mean['NUMBER OF MOTORIST'].round(2)
-print(calculate_agregate_number_of_motorist_min(df))
-
-#
+print("9. 5 ulica z największą liczbą wypadków z 3 stanów z największą liczbą wypadków")
+print(clalculate_accident_per_street_per_borough(df, 5, 3))
